515ed585f3350ac6178780807a6838724ea58213
lcqh2635 <lcqh2635@gmail.com>
2026-06-09 00:26:08 +0800

    安卓手机安装 Linux 系统

--- android/ubuntu/Android 手机刷原生 Linux 系统操作记录.sh
+++ android/ubuntu/Android 手机刷原生 Linux 系统操作记录.sh
@@ -229,6 +229,26 @@ sudo systemctl enable --now docker
 sudo systemctl status docker --no-pager
 
 
+# 1. 查看防火墙状态和规则
+sudo ufw status
+# 2. 如果状态是 active，确保 22 端口（或你自定义的 SSH 端口）是 ALLOW
+# 如果没有，手动放行 22 端口：
+# 1. SSH 端口 (默认 22，如果你改过，请替换为实际端口，如 2222)
+sudo ufw allow 22/tcp
+# 2. 宝塔面板默认端口 (如果你安装时修改过，请替换为实际端口)
+sudo ufw allow 8888/tcp
+# 3. 宝塔 phpMyAdmin 默认端口 (如果你需要用网页管理数据库，建议放行)
+sudo ufw allow 888/tcp
+# HTTP 默认端口
+sudo ufw allow 80/tcp
+# HTTPS 默认端口 (配置 SSL 证书后必须)
+sudo ufw allow 443/tcp
+# 3. 重新加载防火墙
+sudo ufw reload
+# 看所有已放行的端口
+sudo ufw status numbered
+
+
 # 重启系统
 sudo reboot
 
@@ -249,26 +269,6 @@ if [ -f /usr/bin/curl ];then curl -sSO https://download.bt.cn/install/install_pa
 # 浏览器访问以下链接，添加宝塔客服
 # https://www.bt.cn/new/wechat_customer
 #==================================================================
-
-
-# 1. 查看防火墙状态和规则
-sudo ufw status
-# 2. 如果状态是 active，确保 22 端口（或你自定义的 SSH 端口）是 ALLOW
-# 如果没有，手动放行 22 端口：
-# 1. SSH 端口 (默认 22，如果你改过，请替换为实际端口，如 2222)
-sudo ufw allow 22/tcp
-# 2. 宝塔面板默认端口 (如果你安装时修改过，请替换为实际端口)
-sudo ufw allow 8888/tcp
-# 3. 宝塔 phpMyAdmin 默认端口 (如果你需要用网页管理数据库，建议放行)
-sudo ufw allow 888/tcp
-# HTTP 默认端口
-sudo ufw allow 80/tcp
-# HTTPS 默认端口 (配置 SSL 证书后必须)
-sudo ufw allow 443/tcp
-# 3. 重新加载防火墙
-sudo ufw reload
-# 看所有已放行的端口
-sudo ufw status numbered
 
 # 宝塔面板破解
 # https://docs.btkaixin.com/
